6cb910b66c06b1f1c9fd085de92d225d0452dd81
anckrz <anieckarz@op.pl>
2015-08-18 19:46:51 +0200

    pierwszy komit..

---
+++ src/kik/KolkoIKrzyzyk.java
@@ -0,0 +1,13 @@
+package kik;
+import kik.Plansza;
+
+public class KolkoIKrzyzyk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Plansza plansza = new Plansza();
+	System.out.println(plansza.X);
+	plansza.wyswietl();
+	}
+
+}
---
+++ src/kik/Plansza.java
@@ -0,0 +1,26 @@
+package kik;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Plansza {
+
+	final int X = 1;
+	final int O = 0;
+
+	Map<String, Integer> mapA = new HashMap<String, Integer>();
+	int[][] resultInt = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+
+	public Plansza() {
+
+	}
+
+	public void wyswietl() {
+		System.out.println(resultInt[0][0] + " | " + resultInt[0][1] + " | "
+				+ resultInt[0][2] + "\n" + "---------" + "\n" + resultInt[1][0] + " | " + resultInt[1][1] + " | "
+				+ resultInt[1][2] + "\n" + "---------" +"\n" + resultInt[2][0] + " | " + resultInt[2][1] + " | "
+				+ resultInt[2][2]);
+
+	}
+
+}
